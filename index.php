08e279c931bb0c809bd2a0f7c88f8028dd3840c3
Ian MacLeod <imacleod@mail.umw.edu>
2024-09-23 06:50:55 -0400

    Added class

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello";
+class Dog {
+    private string $name;
+    private string $breed;
+
+    public function __construct($name, $breed){
+        $this->name = $name;
+        $this->breed = $breed;
+    }
+}
